126023340b82656e0ffc2fa36a6bbd52be3bc436
davide-rullo <davide.rullo01@gmail.com>
2023-09-13 14:51:34 +0200

    add
    added todos loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
-        }
+            todos: [
+                {
+                    text: "",
+                    done: false
+                },
+
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+
+                {
+                    text: "Chiamare Roberto",
+                    done: false
+                }
+            ]
+        };
     }
 
-    
+
 }).mount('#app')
